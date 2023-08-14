76d68b93f69171d5d491972aa74c70d5c1e51270
Brandon Pyle <mail4brando@gmail.com>
2023-08-13 20:44:44 -0400

    version 1.8.12 - even more metadata / seo stuff

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -2,6 +2,15 @@ import '../../styles/globals.css';
 
 import Header from '../../components/Header.tsx';
 
+import { Metadata } from "next";
+
+export const metadata:Metadata = {
+    robots: {
+        index: true,
+        nocache: false
+    }
+}
+
 export default function RootLayout({
   children,
 }: {
