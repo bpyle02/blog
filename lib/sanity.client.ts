2adff0cbfcdad875ccee80fabcf353edbe459385
bpyle02 <mail4brando@gmail.com>
2023-12-14 18:48:36 -0500

    version 1.8.15 - Updated Sanity

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -1,5 +1,5 @@
-import { createClient } from 'next-sanity';
-import {apiVersion, dataset, projectId} from '../sanity/env'
+import { createClient } from '@sanity/client';
+import { apiVersion, dataset, projectId } from '../sanity/env'
 
 // export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 // export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
